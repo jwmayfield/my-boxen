b0bc300449b9a54ddb5d462899eefdf81d108592
Jason Mayfield <jason@jwmayfield.com>
2015-05-14 07:08:43 -0400

    Install minimum vim bundles to support basics that I'm used to

--- modules/people/manifests/jwmayfield.pp
+++ modules/people/manifests/jwmayfield.pp
@@ -1,10 +1,10 @@
 class people::jwmayfield {
+  include people::jwmayfield::vim
   include people::jwmayfield::zsh
 
-  $my_username      = $::boxen_user
-  $my_dotfilesdir   = "${::boxen_srcdir}/dotfiles"
-  $my_homedir       = "/Users/${my_username}"
-  $my_statusdir     = '/private/var/db'
+  $dotfiles   = "${::boxen_srcdir}/dotfiles"
+  $home       = "/Users/${::luser}"
+  $status     = '/private/var/db'
 
   git::config::global {
     'user.email': value    => 'jason@jwmayfield.com' ;
@@ -16,64 +16,47 @@ class people::jwmayfield {
   }
 
   # Hg config
-  file { "${my_homedir}/.hgext":
-    ensure  => link,
-    target  => "${my_dotfilesdir}/hgext",
+  file { "${home}/.hgext":
+    ensure  => symlink,
+    target  => "${dotfiles}/hgext",
     require => [
-      Repository[$my_dotfilesdir]
+      Repository[$dotfiles]
     ]
   }
-  file { "${my_homedir}/.hgrc":
-    ensure  => link,
-    target  => "${my_dotfilesdir}/hgrc",
+  file { "${home}/.hgrc":
+    ensure  => symlink,
+    target  => "${dotfiles}/hgrc",
     require => [
-      Repository[$my_dotfilesdir]
+      Repository[$dotfiles]
     ]
   }
 
   # Slate window manager
-  file { "${my_homedir}/.slate":
-    ensure  => link,
-    target  => "${my_dotfilesdir}/slate",
-    require => Repository[$my_dotfilesdir]
+  file { "${home}/.slate":
+    ensure  => symlink,
+    target  => "${dotfiles}/slate",
+    require => Repository[$dotfiles]
   }
 
-  # vim config
-  file { "${my_homedir}/.vimrc":
-    ensure  => link,
-    target  => "${my_dotfilesdir}/vimrc",
-    require => Repository[$my_dotfilesdir]
-  }
-
-  vim::bundle { [
-    'altercation/vim-colors-solarized',
-  ]: }
-
-  repository { $my_dotfilesdir:
+  repository { $dotfiles:
     ensure => 'master',
     force  => true,
     source => 'jwmayfield/dotfiles'
   }
 
-  file { $my_statusdir:
+  file { $status:
     ensure  => directory,
     owner   => 'root',
     group   => 'wheel',
-    require => Repository[$my_dotfilesdir]
+    require => Repository[$dotfiles]
   }
-  exec { 'Enable accessibility API':
-    command  => "(echo -n 'a' | sudo tee ${my_statusdir}/.AccessibilityAPIEnabled > /dev/null 2>&1) && chmod 644 ${my_statusdir}/.AccessibilityAPIEnabled",
-    creates  => "${my_statusdir}/.AccessibilityAPIEnabled",
-    provider => shell,
-    user     => root,
-    require  => File[$my_statusdir]
-  }
+
   exec { 'Customize Terminal.app':
-    command  => "bash ${my_dotfilesdir}/osx && touch ${my_statusdir}/.customize-terminal",
-    creates  => "${my_statusdir}/.customize-terminal",
+    command  => "bash ${dotfiles}/osx && touch ${status}/.customize-terminal",
+    creates  => "${status}/.customize-terminal",
     provider => shell,
     user     => root,
-    require  => File[$my_statusdir]
+    require  => File[$status]
   }
 
 }
---
+++ modules/people/manifests/jwmayfield/vim.pp
@@ -0,0 +1,19 @@
+class people::jwmayfield::vim {
+  $dotfiles   = "${::boxen_srcdir}/dotfiles"
+  $home       = "/Users/${::luser}"
+
+  # vim config
+  file { "${home}/.vimrc":
+    ensure  => symlink,
+    target  => "${dotfiles}/vimrc",
+    require => Repository[$dotfiles]
+  }
+
+  vim::bundle { [
+    'altercation/vim-colors-solarized',
+    'kien/ctrlp.vim',
+    'rodjek/vim-puppet',
+    'scrooloose/syntastic',
+    'tomtom/tcomment_vim'
+  ]: }
+}
