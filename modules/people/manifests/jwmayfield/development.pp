b5a3266ef8808c53cd9deef756679da5cdf7dc4f
Jason Mayfield <jason@jwmayfield.com>
2015-10-22 15:26:26 -0400

    Add vagrant omnibus plugin

--- modules/people/manifests/jwmayfield/development.pp
+++ modules/people/manifests/jwmayfield/development.pp
@@ -5,6 +5,7 @@ class people::jwmayfield::development {
 
   vagrant::plugin { 'berkshelf': }
   vagrant::plugin { 'exec': }
+  vagrant::plugin { 'omnibus': }
   vagrant::plugin { 'vmware-fusion':
     license => "file:///${home}/Dropbox/Licenses/vagrant-vmware-fusion.lic"
   }
